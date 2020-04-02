54763de8ec6daf787c804821ce11ffb453fdefab
5yF0Rc3 <62255397+5yF0Rc3@users.noreply.github.com>
2020-04-02 23:54:54 +0200

    new

--- GUI/tkinter_python_REF.py
+++ GUI/tkinter_python_REF.py
@@ -7,9 +7,9 @@ import tkinter as tk
 
 class Application(tk.Frame):
     def __init__(self, master=None):
-        super().__init__(master)
+        tk.Frame.__init__(self, master)
         self.master = master
-        self.grid(column=2,row=3, sticky="n" ) #erzeugt ein mastergrid
+        self.grid(column=2,row=3, sticky=tk.N+tk.S+tk.E+tk.W ) #erzeugt ein mastergrid
         self.columnconfigure(0, weight = 1) #grid setting
         self.rowconfigure(0, weight = 1) #grid setting
         self.pack(pady = 100, padx = 100) #pady/x = Minimum grösse
@@ -43,9 +43,9 @@ class Application(tk.Frame):
         self.text1 = tk.Label(self, text="Text hier !").grid(row = 1, column = 1, sticky="e")
 
         self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["width"] = "80"
-        self.hi_there["command"] = self.say_hi
+        self.hi_there["text"] = "New Item"
+        self.hi_there["width"] = "20"
+        self.hi_there["command"] = self.add_column
         self.hi_there.grid(row = 1, column = 2,sticky="w") #wenn ein grid benutzt wird, kein .pack notwendig
 #        self.hi_there.pack(side="top") #wenn ein grid benutzt wird, kein .pack notwendig
 
@@ -68,7 +68,14 @@ class Application(tk.Frame):
         self.quit.grid(row = 3, column = 3,sticky="e")
 #        self.quit.pack(side="left")
 
-    def 
+
+
+    def add_column(self):
+        self.grid(column=1,row=3, sticky="n" ) #erzeugt ein mastergrid
+        self.DropMenu1 = tk.OptionMenu(self, self.dropvar, *self.choices) #args: erstauswahl, liste. * für Einträge aus liste
+        self.DropMenu1.grid(row = 1, column = 1,sticky="e")
+
+
 
 
     def say_hi(self):
