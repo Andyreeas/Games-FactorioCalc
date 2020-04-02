1ab03b630a102622a12843d1003b1f424cbaa8dd
5yF0Rc3 <62255397+5yF0Rc3@users.noreply.github.com>
2020-04-02 13:55:51 +0200

    Update tkinter_python_test.py

--- tkinter_python_test.py
+++ tkinter_python_test.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+#Doku:
+# https://docs.python.org/3/library/tkinter.html#tkinter-basic-mapping
+
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -10,12 +13,18 @@ class Application(tk.Frame):
     def create_widgets(self):
         self.hi_there = tk.Button(self)
         self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["width"] = "25"
         self.hi_there["command"] = self.say_hi
         self.hi_there.pack(side="top")
 
+        self.hi_there_2 = tk.Button(self)
+        self.hi_there_2["text"] = "Hello World 2 \n(click me)"
+        self.hi_there_2["command"] = self.say_hi
+        self.hi_there_2.pack(side="right")
+
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
-        self.quit.pack(side="bottom")
+        self.quit.pack(side="left")
 
     def say_hi(self):
         print("hi there, everyone!")
