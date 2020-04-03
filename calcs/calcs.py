9364d99cac3eaec66c92636a87ee3b0a450debb7
Andreas Frey <frey.andi@hotmail.com>
2020-04-04 01:28:17 +0200

    added basic functions

---
+++ calcs/calcs.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 21:48:14 2020
+
+@author: freya
+"""
+
+
+import json
+
+class calcs():
+    
+    def __init__(self):
+        database = "../DB/data.json"
+        self.data = json.loads(open(database).read())
+
+    def readJson(self):
+        print(self.data)
+
+    def get_time(self, item):
+        try:
+            print(self.data['Items'][item]["time"])
+        except KeyError:
+            print("Item doesn't exist")
+    
+    def get_amount(self, item):
+        try:
+            print(self.data['Items'][item]["amount"])
+        except KeyError:
+            print("Item doesn't exist")
+    
+    def get_allItemNames(self):
+        for i in self.data['Items']:
+            print(i)
+            
+calc = calcs()
+#calc.readJson()
+#calc.get_time('uranium-processing')
+#calc.get_amount('uranium-processing')
+
+calc.get_allItemNames()
