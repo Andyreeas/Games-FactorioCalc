6b8354a02fd0768d0cf0290d3b3c8dce734de834
Andreas Frey <frey.andi@hotmail.com>
2020-04-05 16:08:15 +0200

    calculating time kinda works

--- calcs/calcTime.py
+++ calcs/calcTime.py
@@ -24,14 +24,14 @@ class calcTime():
         self.em2e = -0.4
         self.em3e = -0.5
         
+        self.pm1v = -0.15
         self.pm1e = 0.4
-        self.pm1v = -0.15
         self.pm1p = 0.04
+        self.pm1v = -0.15
         self.pm1e = 0.6
-        self.pm1v = -0.15
         self.pm1p = 0.06
-        self.pm1e = 0.8
         self.pm1v = -0.15
+        self.pm1e = 0.8
         self.pm1p = 0.10
         
         self.beacon = 0.5
@@ -48,42 +48,68 @@ class calcTime():
         self.furnace1 = 1
         self.furnace2 = 2
         self.efurnace = 2
-    
-    '''
-    TODO
-    '''
-    def time(self, item):
+
+
+    def time(self, item, builder, module1 = 0, module2 = 0, module3 = 0, module4 = 0, number = 0, beamodule1 = 0, beamodule2 = 0):
+        '''
+        To calculate Time of item with boost
+        '''
+        calc = calcTime()
         db = getValues()
         time = int(db.getTime(item))
+        pr = calc.builderPR(builder, module1, module2, module3, module4)
+        boost = calc.beaconBoostPR(number, beamodule1, beamodule2)
+        time = time / (pr * boost)
         
         return time
     
-    # To calculate the Production rate of the builder (assembler, chem plant, usw..)
     def builderPR(self, builder, module1=0, module2=0, module3=0, module4=0):
+        '''
+        To calculate the Production rate of the builder
+        @param builder (assembler, chem plant, usw..)
+        @param modules 1 - 4 (Optional)
+        return float pr
+        '''
         pr = builder
         pr = pr + (pr * (module1 + module2 + module3 + module4))
         return pr
     
-    '''
-    TODO --> To calculate the Power usage of the builder
-    '''
+    def beaconBoostPR(self, number, module1=0, module2=0):
+        '''
+        To calculate the boost of beacon's
+        return float pr%
+        '''
+        i = 0
+        boost = 0
+        while i < number:
+            boost = boost + module1 + module2
+            i = i + 1
+        return boost
+    
+    def power():
+        '''
+        TODO --> To calculate the Power usage
+        '''
+        pass
+    
     def builderPU():
+        '''
+        TODO --> To calculate the Power usage of the builder
+        '''
         pass
     
-    '''
-    TODO --> To calculate the Productivity of the builder
-    '''
+    def beaconBoostPU():
+        '''
+        TODO --> To calculate the Power usage with Beacon's
+        '''
+        pass
+    
     def builderPD():
+        '''
+        TODO --> To calculate the Productivity of the builder
+        '''
         pass
-    
-    '''
-    TODO --> To calculate the boost of a beacon
-    '''
-    def beaconBoost(self, module1, module2):
-            
-        pass
-    
-    
+        
     '''
     # Testing Area
     # 
@@ -93,38 +119,12 @@ class calcTime():
     '''
     def main(self):
         # print(a.time('advanced-circuit', 'true', self.assembler3))
-        print(a.builderPR(self.assembler3, self.sm3v, self.sm3v, self.sm3v, self.sm3v))
+        # pr = a.builderPR(self.assembler3, self.sm3v, self.sm3v, self.sm3v, self.sm3v)
+        time = a.time('accumulator', self.assembler3, self.sm3v, self.sm3v, self.sm3v, self.sm3v, 4,self.sm3v, self.sm3v)
+        return time
 
 if __name__ == "__main__":
     a = calcTime()
-    a.main()
+    print(a.main())
 
        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
