4c7ae39a452353525f4dcb42c4271a802addcf8d
5yF0Rc3 <62255397+5yF0Rc3@users.noreply.github.com>
2020-04-07 17:51:37 +0200

    GUI works for real this time

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,5 +1,6 @@
-# Import GUI
+# Import
 import tkinter as tk 
+from time import sleep
 #Extend Path, for Importing Module in Parent
 import os,sys,inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -40,13 +41,21 @@ class Application(tk.Frame):
         # Load variables and create all widgets
         self.load_variables()
         self.create_widgets()
+        self.updater()
+        
+        #while True:
+        #    sleep(1)
+        #    self.updater()
+        #print("end")
+
+
 
 
     def load_variables(self):
         #   Create Object from Import
-        db = getValues.getValues()
+        self.db = getValues.getValues()
         #Load variables as a list from folder "calcs", file "getValues"
-        self.db_list = db.getAllItemNames()
+        self.db_list = self.db.getAllItemNames()
            # choices defines de dropdownmenu list
         self.choices = self.db_list
            # list_dropvar holds the active value from the dropdownmenu
@@ -64,6 +73,7 @@ class Application(tk.Frame):
         
         
     def create_widgets(self):
+        print("create_widget")
         # Column label description
         self.label_time = tk.Label(self.menue_top, text="Time").grid(
                                    row = 1, column = 2, sticky="e")
@@ -88,20 +98,32 @@ class Application(tk.Frame):
         # -------------------------------
 
 
+
+    def updater(self):
+        # updates the text Labels
+        print("updater")
+#        self.list_item_time[self.listcount - 3].config(text=self.list_item_time_var[0].get())
+        for i in range(self.listcount -2):
+            self.list_item_time[i].config(text=self.db.getTime(self.list_dropvar[i].get()))
+        root.after(500, self.updater)
+
+
+
     def add_line(self):
         # Adds a new row with:
         # list_dropvar initialized as a StringVar, and set value to first in choices
         self.list_dropvar.append(tk.StringVar())
+        #list_dropvar[-1] gets traced, if it get changed it changes all the Label variables
+#        self.list_dropvar[-1].trace("w", self.read_items)
         try:
-            self.list_dropvar[-1].set(self.choices[0])
+            self.list_dropvar[self.listcount -2].set(self.choices[0])
         except:
-            self.list_dropvar[-1].set("Kein Eintrag")
+            self.list_dropvar[self.listcount -2].set("Kein Eintrag")
         # list_item_time_var initialized as a StringVar, get value in read_items()
         self.list_item_time_var.append(tk.StringVar())
-        #list_dropvar[-1] gets traced, if it get changed it changes all the Label variables
-        self.list_dropvar[-1].trace("w", self.read_items)
+        
         #Label-Widget for the list_item_time_var
-        self.list_item_time.append(tk.Label(self.menue_top, textvariable=self.list_item_time_var[-1]))
+        self.list_item_time.append(tk.Label(self.menue_top, text=self.db.getTime(self.list_dropvar[-1].get())))
         self.list_item_time[-1].grid( row = self.listcount, column = 2, sticky="w")
         # Creates dropdownmenu. #Eintrag [0] ist text2 (self.listcount: 2)
         self.list_dropmenu.append(tk.OptionMenu(self.menue_top, self.list_dropvar[-1],
@@ -116,16 +138,23 @@ class Application(tk.Frame):
         if self.listcount > 2:
             self.list_item_time[-1].grid_forget()
             self.list_item_time.pop()
+            self.list_item_time_var.pop()
+            self.list_dropvar.pop(-1)
             self.list_dropmenu[-1].grid_forget()
             self.list_dropmenu.pop()
             self.listcount -= 1
+            print("item_time ", len(self.list_item_time),
+                  ",dropmenu ", len(self.list_dropmenu),
+                  ",drop_var ", len(self.list_dropvar),
+                  
+                  )
         else:
             print("nothing to delete")
 
 
     def read_items(self,*args):
         # If de Dropmenu Variable(list_dropvar) changes, change all the corresponding Label texts
-        self.list_item_time_var[-1].set(db.getTime(self.list_dropvar[-1].get()))
+        self.list_item_time_var[0].set(self.db.getTime(self.list_dropvar[-1].get()))
 
 
     def search_engine(self):
@@ -145,5 +174,9 @@ class Application(tk.Frame):
 
 
 root = tk.Tk()
+root.title = "Factorio Calc"
 app = Application(master=root)
-app.mainloop()
+app.updater()
+root.mainloop()
+
+print("test out")
